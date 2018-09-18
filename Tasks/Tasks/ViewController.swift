b0176f3faae30b0816a2cb0ab7d3cd9cbdfe8b26
Iree García <iree@outlook.com>
2018-09-18 00:05:09 -0500

    Initial layout

---
+++ Tasks/Tasks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Iree García on 9/17/18.
+//  Copyright © 2018 Iree García. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
